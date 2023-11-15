ed6b9c55ca69849897912732b7303c57a5ea753e
hahahaday12 <hahahaday12@naver.com>
2023-11-15 23:46:20 +0900

    Fix: 타입수정

--- src/components/custom/customCommon.tsx
+++ src/components/custom/customCommon.tsx
@@ -1,50 +1,49 @@
-
-interface ItemUsername {
-  username: string;
-  status: 'PENDING' | 'APPROVE' | 'REJECT' | 'CANCELLED';
+export type ItemUsername = {
+  username: string
+  status: 'PENDING' | 'APPROVE' | 'REJECT' | 'CANCELLED'
 }
 
 export const getTitleWithStatus = (item: ItemUsername): any => {
-  if(item.username){
+  if (item.username) {
     switch (item.status) {
       case 'PENDING':
-        return `${item.username} (승인 대기)`;
+        return `${item.username} (승인 대기)`
       case 'APPROVE':
-        return `${item.username} (승인 완료)`;
+        return `${item.username} (승인 완료)`
       case 'REJECT':
-        return `${item.username} (승인 거절)`;
+        return `${item.username} (승인 거절)`
       case 'CANCELLED':
-      return `${item.username} (승인 취소)`;
+        return `${item.username} (승인 취소)`
       default:
-        return item.username;
+        return item.username
     }
   }
-};
+}
 
 export const getMyTitleWithStatus = (item: ItemUsername): string => {
   switch (item.status) {
     case 'PENDING':
-      return `승인대기`;
+      return `승인대기`
     case 'APPROVE':
-      return `승인`;
+      return `승인`
     case 'REJECT':
-      return `반려`;
+      return `반려`
     default:
-      return `승인대기`;
+      return `승인대기`
   }
-};
+}
 
-export const convertStatusToText = (status:string) =>  {
+export const convertStatusToText = (status: string) => {
   switch (status) {
     case 'PENDING':
-      return '승인대기';
+      return '승인대기'
     case 'APPROVE':
-      return '승인완료';
+      return '승인완료'
     case 'REJECT':
-      return '승인거절';
+      return '승인거절'
     case 'CANCELLED':
-      return '승인취소';
+      return '승인취소'
     default:
-      return status;
+      return status
   }
 }
